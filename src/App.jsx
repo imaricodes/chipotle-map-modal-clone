c50d6c37a98ebdd6a6e95097ad5b6976563e9149
Imari Childress <imaricodes@gmail.com>
2023-01-04 09:48:54 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='h-full'>
+        This is an app component
+        </div>
+    </div>
+  )
+}
+
+export default App
